650f11968ba497149e202a404c9afb58e0952c50
andres-f-ariza <arizaandres8@gmail.com>
2023-02-21 12:42:28 -0800

    fix:C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,18 +1,18 @@
 /*The first line of code writes the error message to the
- *standard error stream using the write() function. The
- *message is enclosed in double quotes and contains a newline
- *character (\n) at the end to ensure the message is printed
- *on a new line. The STDERR_FILENO constant specifies that
- *the message should be written to the standard error stream.
- *The second line of code returns an integer value of 1, 
- *indicating an error occurred. This return value is commonly 
- *used to indicate a non-zero exit status for the program, 
- *which can be useful for shell scripts or other programs 
- *that execute this program as a subprocess.
- *In summary, the main() function in this program writes an 
- *error message to the standard error stream and returns a 
- *non-zero exit status to indicate an error occurred.
- */
+*standard error stream using the write() function. The
+*message is enclosed in double quotes and contains a newline
+*character (\n) at the end to ensure the message is printed
+*on a new line. The STDERR_FILENO constant specifies that
+*the message should be written to the standard error stream.
+*The second line of code returns an integer value of 1,
+*indicating an error occurred. This return value is commonly
+*used to indicate a non-zero exit status for the program,
+*which can be useful for shell scripts or other programs
+*that execute this program as a subprocess.
+*In summary, the main() function in this program writes an
+*error message to the standard error stream and returns a
+*non-zero exit status to indicate an error occurred.
+*/
 #include <unistd.h>
 int main(void)
 {
