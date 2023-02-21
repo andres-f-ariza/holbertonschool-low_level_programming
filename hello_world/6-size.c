2e58720ee6504f7cff09c9c74f234dd1bbffe4b1
andres-f-ariza <arizaandres8@gmail.com>
2023-02-21 08:20:41 -0800

    fix:.C program that prints the size of various types on the computer it is compiled and run on. Betty coding style

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,7 +4,7 @@
 */
 #include <stdio.h>
 int main(void)
-{  
+{
 printf("Size of a char: %lu byte(s)\n", (int)sizeof(char));
 printf("Size of an int: %lu byte(s)\n", (int)sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", (int)sizeof(long int));
