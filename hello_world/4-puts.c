f29e5b13d3ee69c7a97262d4e4f816d5abf91264
andres-f-ariza <arizaandres8@gmail.com>
2023-02-21 07:17:46 -0800

    fix:C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.under betty coding style

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,19 +1,23 @@
 /**
+
  * main - Prints "\"Programming is like building a multilingual puzzle"
+
  * Return: 0
+
  */
+
+
 
 #include <stdio.h>
 
-/**
-*The reason for including the header file <stdio.h> in a C program is to provi*de access to the standard input/output library functions. This header file def*ines a number of functions that are commonly used for input and output operati*ons in C programs, including printf, scanf, puts, gets, fopen, fclose, fread, *fwrite, and many others
-.*/
 
 
-int main(void) {
+int main(void)
+  
+{
   
   puts("\"Programming is like building a multilingual puzzle");
   
-  return 0;
+  return (0);
   
 }
