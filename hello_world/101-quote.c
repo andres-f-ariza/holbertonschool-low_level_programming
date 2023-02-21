0c45b01260264e018a80000fb6db75937bce7500
andres-f-ariza <arizaandres8@gmail.com>
2023-02-21 12:21:20 -0800

    fix:C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,7 +1,17 @@
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
 int main(void)
 {
-write(STDERR_FILENO, "Error: and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ssize_t bytes_written = write(STDERR_FILENO, "Error: ", strlen("Error: "));
+if (bytes_written < 0) {
+return 1;
+}
+bytes_written = write(STDERR_FILENO, message, strlen(message));
+if (bytes_written < 0) {
+return 1;
+}
 return 1;
 }
 
