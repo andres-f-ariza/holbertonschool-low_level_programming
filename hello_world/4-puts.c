96ccdc26243d8896358995546d27ebe1ad26e454
andres-f-ariza <arizaandres8@gmail.com>
2023-02-21 07:05:10 -0800

    feat:C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.

---
+++ hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/**
+ * main - Prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+#include <stdio.h>
+
+/**
+*The reason for including the header file <stdio.h> in a C program is to provi*de access to the standard input/output library functions. This header file def*ines a number of functions that are commonly used for input and output operati*ons in C programs, including printf, scanf, puts, gets, fopen, fclose, fread, *fwrite, and many others
+.*/
+
+
+int main() {
+  
+  puts("Programming is like building a multilingual puzzle");
+  
+  return 0;
+  
+}
