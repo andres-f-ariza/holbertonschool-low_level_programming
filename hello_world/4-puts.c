919ff09a3c17f293017b37242658e67b4199d439
andres-f-ariza <arizaandres8@gmail.com>
2023-02-21 07:46:51 -0800

    fix:C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line. Betty coding style

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,13 +3,8 @@
  *Return: 0
  */
 #include <stdio.h>
-
 int main(void)
- 
-{
-  
-  puts("\"Programming is like building a multilingual puzzle");
-  
+{  
+  puts("\"Programming is like building a multilingual puzzle"); 
   return (0);
-  
 }
