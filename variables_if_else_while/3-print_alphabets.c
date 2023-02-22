498a3a1d011e4f316d2d6969fd8ed9cc68f1d6df
andres-f-ariza <arizaandres8@gmail.com>
2023-02-22 09:29:29 -0800

    fix: file3 Modifications to Betty coding style

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,16 @@
 /**
 *main - Prints lowercase alphabet
 *Return: 0
-*first while loop prints out characters ASCII, 
+*first while loop prints out characters ASCII,
 *from 97 to 122. These values correspond to
 *lowercase "a" to "z".
-*second while loop prints out characters ASCII, 
+*second while loop prints out characters ASCII,
 *from 65 to 90. These values correspond to
 *lowercase "A" to "Z".
 */
 #include <stdio.h>
-int main(void){
+int main(void)
+{
 char c;
 c = 97;
 while (c < 123)
