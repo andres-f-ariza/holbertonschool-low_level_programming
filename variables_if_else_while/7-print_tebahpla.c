013e6d8fcfbc5da6d32537087a99382c4e0da9d5
andres-f-ariza <arizaandres8@gmail.com>
2023-02-22 10:42:56 -0800

    feat:file7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+/**
+* main - Prints lowercase alphabet in reverse
+*while loop that prints out values from initialized c
+*in 122 to 97, decrasing by step 1. These
+*values correspond to the lowercase letters
+*in English
+*Return: 0
+*/
+
+#include <stdio.h>
+int main(void)
+{
+char c;
+c = 122;
+while (c > 96)
+{
+putchar(c);
+c--
+}
+putchar('\n');
+return (0);
+}
