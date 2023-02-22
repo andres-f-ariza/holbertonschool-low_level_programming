519a9d3d5bdb7dedbbcd626582ef22678c3e1e73
andres-f-ariza <arizaandres8@gmail.com>
2023-02-22 10:30:49 -0800

    feat:file6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+/**
+* main - Prints numbers 
+* Return: 0
+*while loop
+*prints out characters from 0
+*to 9, with an increase of 1, as soon as it finishes,
+*it makes a new line
+*/
+
+#include <stdio.h>
+int main(void)
+{
+int c;
+c = '0';
+while (c < '10')
+{
+putchar(c);
+c++;}
+putchar('\n');
+return (0);
+}
