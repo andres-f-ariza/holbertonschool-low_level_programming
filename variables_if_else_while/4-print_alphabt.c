1fbb6a5131512448df4dae18a1108a85cebb9d47
andres-f-ariza <arizaandres8@gmail.com>
2023-02-22 09:50:51 -0800

    fix:file4 compilation

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,19 @@
 /**
-* main - Prints lowercase alphabet* Return: 0
-*for loop prints out the values*97 to 122 in the ASCII table that correspond
-*to the lowercase English alphabet*from "a" to "z".+inside
+* main - Prints lowercase alphabet
+* Return: 0
+*for loop prints out the values
+*97 to 122 in the ASCII table that correspond
+*to the lowercase English alphabet
+*from "a" to "z".inside
 *the for loop we find an
-*if conditional statement which*commands to not print out
+*if conditional statement which
+*commands to not print out
 *ASCII values when the for
 *loop reaches 101
 *"e" or 113 "q"
 */
-#include <stdio.h>int
-main(void){
+#include <stdio.h>
+int main(void){
 char c;
 for (c = 97; c < 123; c++)
 {
