7092385535846b7caefd15698ee46880aea43df0
andres-f-ariza <arizaandres8@gmail.com>
2023-02-22 09:11:32 -0800

    feat:3 file

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,48 @@
+/**
+*main - Prints lowercase alphabet
+*Return: 0
+*first while loop prints out characters ASCII, 
+*from 97 to 122. These values correspond to
+*lowercase "a" to "z".
+*second while loop prints out characters ASCII, 
+*from 65 to 90. These values correspond to
+*lowercase "A" to "Z".
+*/
+
+#include <stdio.h>
+
+int main(void)
+  
+{
+  
+  char c;
+  
+  c = 97;
+  
+  while (c < 123)
+    
+    {
+      
+      putchar(c);
+      
+      c++;
+      
+    }
+  
+  c = 65;
+  
+  while (c < 91)
+    
+    {
+      
+      putchar(c);
+      
+      c++;
+      
+    }
+  
+  putchar('\n');
+  
+  return (0);
+  
+}
