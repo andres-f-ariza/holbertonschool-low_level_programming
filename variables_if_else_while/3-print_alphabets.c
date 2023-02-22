fea140fddef0f4f15a5b605282183b818c3151a3
andres-f-ariza <arizaandres8@gmail.com>
2023-02-22 09:20:52 -0800

    fix: 3file
    Modifications to Betty coding style

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,41 +8,21 @@
 *from 65 to 90. These values correspond to
 *lowercase "A" to "Z".
 */
-
 #include <stdio.h>
-
-int main(void)
-  
+int main(void){
+char c;
+c = 97;
+while (c < 123)
 {
-  
-  char c;
-  
-  c = 97;
-  
-  while (c < 123)
-    
-    {
-      
-      putchar(c);
-      
-      c++;
-      
-    }
-  
-  c = 65;
-  
-  while (c < 91)
-    
-    {
-      
-      putchar(c);
-      
-      c++;
-      
-    }
-  
-  putchar('\n');
-  
-  return (0);
-  
+putchar(c);
+c++;
+}
+c = 65;
+while (c < 91)
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }
