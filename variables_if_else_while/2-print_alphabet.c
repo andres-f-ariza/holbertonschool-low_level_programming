89478666cb4d57e4dd74a91da2df0fefd7099297
andres-f-ariza <arizaandres8@gmail.com>
2023-02-22 08:37:04 -0800

    fix:file 2. Modifications to Betty coding style

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,7 @@
 /**
  * main - Prints lowercase alphabet
+ *while loop to print out all the ASCII characters with values from 97 to 122.  *Variable "c" is initialized to 97, and the loop continues to execute as lon\  *g as "c" is less than 123. Inside the loop, the putchar() function is used to *print the character corresponding to the current value of "c". The value of " *c" is then incremented by 1 at the end of each iteration. The values 97 to 
+ *122 correspond to the lowercase English alphabet from "a" to "z".
  * Return: 0
  */
 
@@ -8,13 +10,9 @@
 #include <stdio.h>
 
 
-
 int main(void)
 {
 char c;
-/**
-*while loop to print out all the ASCII characters with values from 97 to 122. T*he variable "c" is initialized to 97, and the loop continues to execute as lon*g as "c" is less than 123. Inside the loop, the putchar() function is used to *print the character corresponding to the current value of "c". The value of "c*" is then incremented by 1 at the end of each iteration. The values 97 to 122 *in the ASCII table correspond to the lowercase English alphabet from "a" to "z*".
- */
 c = 97;
 while (c < 123)
 {
