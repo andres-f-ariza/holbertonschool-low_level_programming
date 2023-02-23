5c2de8dd3de94e388b64492dcfc3ba20d9722717
andres-f-ariza <arizaandres8@gmail.com>
2023-02-23 12:06:18 -0800

    fix:file10..Modifications towards Betty coding style

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,6 +1,6 @@
 /**
-* add - adds two integers and returns the result.
-* @a: First number to add
+*'add' - adds two integers and returns the result.
+*@a: First number to add
 *@b: second number to add*Return: a+b
 */
 
