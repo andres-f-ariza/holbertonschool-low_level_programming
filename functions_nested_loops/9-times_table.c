08402089cc9344d7556eaab7e92ca03713e6bcaf
andres-f-ariza <arizaandres8@gmail.com>
2023-02-23 10:43:52 -0800

    fix:file9.compilation

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -81,7 +81,7 @@ if (b != 9 && ((b + 1) * a) < 10)
 {								       {
 double_space_comma();
 }
-else if (b != 9 && ((b + 1) * a) >= 10)
+}else if (b != 9 && ((b + 1) * a) >= 10)
 {
 single_space_comma();
 }
