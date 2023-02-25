839243a8ff0e396b12295964e2c735b5bebb43f4
andres-f-ariza <arizaandres8@gmail.com>
2023-02-24 19:20:01 -0800

    fix:files9,10,11.Modifications towards Betty coding style

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,9 +1,11 @@
+#include "main.h"
+#include <stdio.h>
 /**
-*add - adds two integers and returns the result.
-*@a: First number to add
-*@b: second number to add*Return: a+b
+* add - adds two integers and returns the result.
+* @a: First number to add
+* @b: second number to add
+* Return: a+b
 */
-
 int add(int a, int b)
 {
 return (a + b);
