d30360b931d0ed18ea62d03daa635433bcc97e48
andres-f-ariza <arizaandres8@gmail.com>
2023-03-02 10:34:32 -0800

    feat:file.0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -3,24 +3,24 @@
 *_strcat - concatenate two strings
 *@src - source pointer
 *@dest - destiny pointer
-*Return:dest
+OA*Return:dest
 */
 char *_strcat(char *src, char *dest)
 {
 int i;
 int j;
-i=0;
-while(dest[i] != '\0')
+i = 0;
+while (dest[i] != '\0')
 {
 i++;
 }
-j=0;
-while(src[j] != '\0')
+j = 0;
+while (src[j] != '\0')
 {
-dest[i]=src[j];
+dest[i] = src[j];
 i++;
 j++;
 }
-dest[i]='\0';
-return(dest);
+dest[i++] = '\0';
+return (dest);
 }
