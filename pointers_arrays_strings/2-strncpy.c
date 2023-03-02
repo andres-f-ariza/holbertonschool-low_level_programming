a174c784b752eeff2db22dfe3d0926acfbc5eaa7
andres-f-ariza <arizaandres8@gmail.com>
2023-03-02 12:28:52 -0800

    fix:file2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string.
+ * @s: Pointer to string to measure
+ * Return: Lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int len;
+
+	i = 0;
+	len = 0;
+	while (*(s + i) != '\0')
+	{
+		len = len + 1;
+		i++;
+	}
+	return (len);
+}
+
+/**
  * _strncpy - copies a string, with at most n bytes from src
  * @dest: Pointer to destination string
  * @src: Pointer to source string
@@ -8,14 +29,22 @@
  * Return: Pointer to resulting string dest
  */
 
-strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
+	int lensrc;
+	int lendest;
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	lensrc = _strlen(src);
+	lendest = _strlen(dest);
+	i = 0;
+	while (i < n && i < lendest)
 	{
-		dest[i] = src[i];
-	i++;
+		if (*(dest + i) != '\0' && i < lensrc)
+			*(dest + i) = *(src + i);
+		else
+			*(dest + i) = '\0';
+		i++;
 	}
-	return dest;
+	return (dest);
 }
