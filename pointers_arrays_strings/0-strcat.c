3909ffd4fb10ee97a83599927a12cdfaf70fb1ed
andres-f-ariza <arizaandres8@gmail.com>
2023-03-02 11:06:11 -0800

    fix:file0-strcat.output

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,28 @@
 #include "main.h"
 /**
-*_strcat - concatenate two strings
-*@src: source pointer
-*@dest: destiny pointer
-*Return:void
-*/
-char *_strcat(char *src, char *dest)
+ * _strcat - concatenates two strings
+ * @dest: source pointer
+ * @src: destiny pointers
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
 {
-int i;
-int j;
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
-j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0';
-return (dest);
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
