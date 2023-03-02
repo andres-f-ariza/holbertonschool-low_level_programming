068b6a893d698f1480ab4ccf4eda032bf9d81ef0
andres-f-ariza <arizaandres8@gmail.com>
2023-03-02 11:57:26 -0800

    fix:file1.includelibrary

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,12 @@
 #include "main.h"
+#include <string.h>
 char *_strncat(char *dest, char *src, int n)
 {
 	int lendest;
 	int lensrc;
 	int i;
 
-	lendest = _strlen(dest);
+	lendest = strlen(dest);
 	lensrc = _strlen(src);
 	i = 0;
 	while (i < n && i < lensrc)
