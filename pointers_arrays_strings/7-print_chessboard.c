36bbd4e7fe78137c8d06b098b3c88704874f1cfe
andres-f-ariza <arizaandres8@gmail.com>
2023-03-03 11:10:29 -0800

    feat:file7

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints the chessboard.
- * @a: Chessboard to print
- */
+* print_chessboard - prints the chessboard.
+* @a: Chessboard to print
+*/
 
 void print_chessboard(char (*a)[8])
 {
 	int i;
 	int j;
-
+/*
+ *sssss
+ */
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
