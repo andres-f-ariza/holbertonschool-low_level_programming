2b26f053ea3fbbeeb52e6adf04579714f25feca4
andres-f-ariza <arizaandres8@gmail.com>
2023-03-08 14:32:19 -0800

    fix:file101.Modifications towards Betty coding style

--- recursion/101-wildcmp.c
+++ recursion/101-wildcmp.c
@@ -18,18 +18,17 @@ if (*s2 == '*')
  *pointer value and the next s2 pointer value
  *to match any string that could come after the * character
  */
-if (wildcmp(s1, s2 + 1)) {
+if (wildcmp(s1, s2 + 1))
 return 1;
-}
-
 /**
  *We recursively call wildcmp() with the next s1
  *pointer value and the current s2 pointer value
  *to match any string that could come before the * character
  */
-if (*s1 != '\0' && wildcmp(s1 + 1, s2)) {
+if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+{
 return 1;
-}
+
 
 /**
  *If neither of the above recursive calls return 1
@@ -42,12 +41,13 @@ return 0;
  *If the current character of s1 and s2 match,
  *we move on to the next character
  */
-	if (*s1 == *s2) {
-		return wildcmp(s1 + 1, s2 + 1);
-	}
+if (*s1 == *s2)
+{
+return wildcmp(s1 + 1, s2 + 1);
+}
 /**
  *If the current character of s1 and s2 don't
  *match, the strings cannot be considered identical
  */
-	return 0;
+return 0;
 }
