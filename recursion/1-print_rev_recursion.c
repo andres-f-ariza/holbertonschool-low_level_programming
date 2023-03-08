629a73c5b67d1132e7f366757706dee202516eb0
andres-f-ariza <arizaandres8@gmail.com>
2023-03-08 13:03:35 -0800

    feat:file1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -5,11 +5,11 @@
  *@s: String to print
  */
 
-void print_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if(*s != '\0')
 	{
-		print_rev_recursion(s+1);
+		_print_rev_recursion(s+1);
 _putchar(*s);
 	}
 }
