692187d93058cd6fcaa7adeede6dda0a9a565802
andres-f-ariza <arizaandres8@gmail.com>
2023-03-17 14:41:14 -0700

    fix:file3.compilation

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,39 +1,48 @@
 #include "main.h"
 #include <stddef.h>
 #include <stdlib.h>
-
 /**
- * alloc_grid - returns a pointer to a 2 dimensional array of integers.
- * @width: Width of the array
- * @height: height of the array
- * Return: Pointer to the array initialized to 0
+ * alloc_grid - allocate memory for a 2D array of integers
+ * @width: width of the 2D array
+ * @height: height of the 2D array
+ *
+ * Return: pointer to 2D array of integers (Success), NULL (Failure)
  */
-
 int **alloc_grid(int width, int height)
 {
-	int i;
-	int j;
-	int **ret;
+	int i; // declare integer variable "i"
+	int j; // declare integer variable "j"
+	int **ret; // declare pointer variable "ret"
 
+	/* allocate memory for array of pointers to int */
 	ret = (int **)malloc(sizeof(int *) * height);
+
+	/* check if memory allocation was successful or if width and height are invalid */
 	if (ret == NULL || (width * height) <= 0)
 		return (NULL);
+
+	/* allocate memory for each row of the 2D array */
 	for (i = 0; i < height; i++)
 	{
 		ret[i] = (int *)malloc(sizeof(int) * width);
+		/* check if memory allocation was successful */
 		if (ret[i] == NULL)
 		{
+			/* free memory for previous rows that were allocated */
 			while (i >= 0)
 			{
 				free(ret[i]);
 				i--;
 			}
+			/* free memory for array of pointers to int */
 			free(ret);
 			return (NULL);
 		}
+		/* initialize values of each element in the row to 0 */
 		for (j = 0; j < width; j++)
 			ret[i][j] = 0;
 	}
-	return (ret);
 
+	return (ret); // return pointer to 2D array of integers
 }
+
