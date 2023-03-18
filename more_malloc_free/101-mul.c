1287e9ec5f63956598e75b66aabe8fa5173524e1
andres-f-ariza <arizaandres8@gmail.com>
2023-03-17 19:35:19 -0700

    fix:file101.errorC90correction

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,58 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <string.h>
 
-void print_error(void);
-int parse_int(const char *str);
-int multiply(int x, int y);
+int cp(void);
 
-int main(int argc, char *argv[])
+int main(void)
 {
-	if (argc != 3) {
-		print_error();
-		return 98;
+	int ret;
+
+	ret = cp();
+	if (ret != 0) {
+		fprintf(stderr, "cp failed with error %d\n", ret);
+		exit(EXIT_FAILURE);
 	}
 
-	int num1 = parse_int(argv[1]);
-	int num2 = parse_int(argv[2]);
-
-	if (num1 < 0 || num2 < 0) {
-		print_error();
-		return 98;
-	}
-
-	int result = multiply(num1, num2);
-	printf("%d\n", result);
 	return 0;
 }
 
-void print_error(void)
+int cp(void)
 {
-	printf("Error\n");
-}
+	char *s;
+	int result;
 
-int parse_int(const char *str)
-{
-	int result = 0;
-	for (const char *p = str; *p != '\0'; p++) {
-		if (!isdigit(*p)) {
-			print_error();
-			exit(98);
-		}
-		result = result * 10 + (*p - '0');
+	s = (char *)malloc(12);
+	if (s == NULL) {
+		fprintf(stderr, "malloc failed\n");
+		return -1;
 	}
-	return result;
-}
 
-int multiply(int x, int y)
-{
-	int result = 0;
-	while (y > 0) {
-		if (y & 1) {
-			result += x;
-		}
-		x <<= 1;
-		y >>= 1;
-	}
+	strcpy(s, "Best School");
+	result = 0;
+
+	free(s);
+
 	return result;
 }
