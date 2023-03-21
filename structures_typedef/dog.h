cda172771d3bf2c7e48dac067dd5d336bb6699ac
andres-f-ariza <arizaandres8@gmail.com>
2023-03-21 13:50:17 -0700

    feat:file0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - Structure to identify a dog
+ * @name: Type char, dog's name
+ * @age: Type float, dog's age
+ * @owner: Type char, dog's owner name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
