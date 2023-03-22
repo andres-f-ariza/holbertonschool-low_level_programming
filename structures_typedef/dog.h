e8c3232133a995375dc72e90402f81151fde37e6
andres-f-ariza <arizaandres8@gmail.com>
2023-03-22 09:39:16 -0700

    feat:file3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - Structure to identify a dog
  * @name: Type char, dog's name
@@ -11,3 +14,22 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedef for dog struct
+ */
+typedef struct dog dog_t;
+
+/**
+ * Prototypes - Prototypes for functions
+ * @d: pointer to dog info
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
