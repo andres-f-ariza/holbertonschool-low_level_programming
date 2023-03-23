30f424bb01a2623dc127cd0229149d967f491500
andres-f-ariza <arizaandres8@gmail.com>
2023-03-23 16:37:45 -0700

    feat:file2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+	
 /**
  * int_index - searches for an integer using the cmp function
  * @array: array to look into
@@ -8,20 +8,24 @@
  * Return: Index of the first match, or -1 if there's no match
  * or if size is 0 or less.
  */
+int int_index(int *array, int size, int (*cmp)(int)) {
+	int i; // Declare a counter variable.
 
-int int_index(int *array, int size, int (*cmp)(int))
-{
-	int i;
-
+	// Check if the array size is 0 or less.
 	if (size <= 0)
 		return (-1);
-	if (array !=  NULL && cmp != NULL)
-	{
-		for (i = 0; i < size; i++)
-		{
+
+	// Check that the array and function pointer are not NULL.
+	if (array !=  NULL && cmp != NULL) {
+		// Loop over each element in the array.
+		for (i = 0; i < size; i++) {
+			// Call the provided function on the current element of the array.
+			// If the result is not 0, return the current index.
 			if (cmp(array[i]) != 0)
 				return (i);
 		}
 	}
+
+	// If no match was found, or if the array or function pointer is NULL, return -1.
 	return (-1);
 }
