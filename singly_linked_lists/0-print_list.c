dfde305423769caa84729df34f9e21b36a9e7260
andres-f-ariza <arizaandres8@gmail.com>
2023-04-05 12:18:37 -0700

    feat:file1

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,22 +7,45 @@
  */
 
 size_t print_list(const list_t *h)
+
+/**
+ *function declaration in C that takes a pointer to the head(h)
+ *of a linked list and returns the number of nodes in the list.
+ *The const keyword, makes a variable read-only so it is before the list_t type indicates that the function will not modify the contents of the linked list.
+ */
 {
 	size_t ret;
-
+	/* Set the variable to zero */
 	ret = 0;
+
+	/* Loop through the list until the end is reached (h == NULL) */
 	while (h != NULL)
 	{
+		/* If the current node's string is NULL, print "(nil)" */
 		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
+
+	       {
+/**
+ *              The [0] part of the printed string is used to indicate the length of the string, which is 0 in this case.
+ *The reason for including [0] in the printed string is to maintain a consistent format for printing out the contents of linked list nodes.
+ */
+		       
+		       printf("[0] (nil)\n");
 			h = h->next;
 			ret += 1;
 			continue;
 		}
+
+		/* Otherwise, print the length of the string and the string itself */
 		printf("[%i] %s\n", h->len, h->str);
+
+		/* Increment the node count */
 		ret += 1;
+
+		/* Move on to the next node */
 		h = h->next;
 	}
+
+	/* Return the number of nodes in the list */
 	return (ret);
 }
