0893d3c129b9dd5b0e22857dd901a6a56a46e07b
andres-f-ariza <arizaandres8@gmail.com>
2023-04-06 05:37:12 -0700

    feat:file100

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -7,10 +7,16 @@
 
 void free_list(list_t *head)
 {
+	/* check if head exists and has a next node */
 	if (head && head->next)
+		/* recursively call free_list on the next node */
 		free_list(head->next);
+	/* check if head exists and has a string */
 	if (head && head->str)
+		/* free the string */
 		free(head->str);
+	/* check if head exists */
 	if (head)
+		/* free the head node */
 		free(head);
 }
