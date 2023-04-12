8136953129cfe9e585083c18c16c12113ae5e899
andres-f-ariza <arizaandres8@gmail.com>
2023-04-12 13:19:44 -0700

    fix:file3.commented

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,6 +7,10 @@
  * @buff: Buffer to free
  */
 
+/**
+ *hich is a helper function that will be used later in the code. It takes in
+ *two file descriptors (fd1 and fd2) and a buffer (buff) as arguments.
+ */
 void closeandfree(int fd1, int fd2, char *buff)
 {
 	close(fd1);
@@ -23,9 +27,20 @@ void closeandfree(int fd1, int fd2, char *buff)
 
 int copy_file(const char *file_from, const char *file_to)
 {
+	/**
+	 *which takes in two file names (file_from and file_to) as arguments and
+	 *returns an integer. It will copy the contents of file_from to file_to
+	 */
 	int fd1, fd2, lenfrom, lento, aux;
 	char *buff;
-
+	/**
+	 *These lines declare several variables that will be used in the
+	 *copy_file() function. fd1 and fd2 are file descriptors for the source
+	 *and destination files, respectively. lenfrom and lento are the length
+	 *of the data read from and written to the files, respectively. aux is
+	 *used to store the result of various system calls, and buff is a buffer
+	 *that will hold the data read from file_from.
+	 */
 	lenfrom = 1;
 	fd1 = open(file_from, O_RDONLY);
 	if (fd1 == -1)
@@ -33,40 +48,60 @@ int copy_file(const char *file_from, const char *file_to)
 	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
 		return (-2);
-	buff = (char *)malloc(sizeof(char) * 1024);
-	if (buff == NULL)
-		return (0);
-	while (lenfrom > 0)
-	{
-		lenfrom = read(fd1, buff, 1024);
-		if (lenfrom == -1)
-		{
-			closeandfree(fd1, fd2, buff);
-			return (-1);
-		}
-		lento = write(fd2, buff, lenfrom);
-		if (lento == -1)
-		{
-			closeandfree(fd1, fd2, buff);
-			return (-2);
-		}
-	}
-	aux = close(fd1);
-	if (aux == -1)
-		return (fd1);
-	aux = close(fd2);
-	if (aux == -1)
-		return (fd2);
-	free(buff);
-	return (1);
-}
-
+	/**
+	 *These lines open the source and destination files using open() system
+	 *call. O_RDONLY and O_CREAT | O_WRONLY | O_TRUNC are the flags used to
+	 *open the files. If there is an error opening the files, the function
+	 *returns -1 or -2, respectively.
+	 */
+buff = (char *)malloc(sizeof(char) * 1024);
+if (buff == NULL)
+return (0);
 /**
- * main - Main function for cp
- * @argc: argument counter
- * @argv: argument vector
- * Return: Always 0
+ *This line allocates memory for the buff buffer using the malloc() function. If
+ *malloc() returns NULL, the function returns 0.
  */
+while (lenfrom > 0)
+{
+	lenfrom = read(fd1, buff, 1024);
+	if (lenfrom == -1)
+	{
+		closeandfree(fd1, fd2, buff);
+		return (-1);
+	}
+	lento = write(fd2, buff, lenfrom);
+	if (lento == -1)
+	{
+		closeandfree(fd1, fd2, buff);
+		return (-2);
+	}
+}
+/**
+ *This loop reads data from file_from in 1024-byte chunks using the read()
+ *system call and stores it in buff. It then writes the data to file_to using
+ *the write() system call. If there is an error during either of these
+ *operations, the function returns -1 or -2, respectively.
+ */
+aux = close(fd1);
+if (aux == -1)
+	return (fd1);
+aux = close(fd2);
+if (aux == -1)
+	return (fd2);
+free(buff);
+return (1);
+/**
+ *These lines close the file descriptors fd1 and fd2 using the close() system
+ *call and free the memory allocated for buff using the free() function. If
+ *there is an error during either of the close() calls, the function returns the
+ *corresponding file descriptor. If everything was successful, the function
+ *returns 1.
+/**
+* main - Main function for cp
+* @argc: argument counter
+* @argv: argument vector
+* Return: Always 0
+*/
 
 int main(int argc, char **argv)
 {
