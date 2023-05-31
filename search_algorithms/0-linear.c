27feb7c58417364789708cb1f5bab33150ebdb1e
andres-f-ariza <arizaandres8@gmail.com>
2023-05-30 18:53:12 -0700

    feat:files

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+/**
+ * linear_search - Performs linear search algorithm on an array
+ * @array: pointer to first element of array to search in
+ * @size: Number of elements in array
+ * @value: Value to search
+ * Return: First index where value is found, or -1 if not found
+ */
+
+#include "search_algos.h"
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			break;
+	}
+	if (i == size)
+		i = -1;
+	return (i);
+}
---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,17 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+/***
+ * Common libraries
+ */
+
+#include <stdio.h>
+
+/***
+ * Prototypes
+ */
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
